2ffd64a438a73deb869924476683cba56791c6bc
CAB426 <CAB426@users.noreply.github.com>
2020-05-14 21:30:20 -0700

    Update const.py

--- owlet/const.py
+++ owlet/const.py
@@ -1,8 +1,8 @@
 """Constants for Owlet component."""
 SENSOR_OXYGEN_LEVEL = 'oxygen_level'
 SENSOR_HEART_RATE = 'heart_rate'
-SENSOR_SOCK_OFF = 'sock_connection'
-SENSOR_BATTERY_CHARGING = 'battery_charging'
+SENSOR_SOCK_ON = 'sock_connection'
+SENSOR_BATTERY_CHARGING = 'charge_status'
 SENSOR_BATTERY_LEVEL = 'batt_level'
 SENSOR_SOCK_CONNECTION = 'sock_connection'
 SENSOR_BASE_STATION = 'base_station_on'
